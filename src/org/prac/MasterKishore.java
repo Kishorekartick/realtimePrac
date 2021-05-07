fff784f4bec532e13a118c4e950c67803853d84c
Kishorekartick <kishoreharish360@gmail.com>
2021-05-07 13:43:50 +0530

    prac1

---
+++ src/org/prac/MasterKishore.java
@@ -0,0 +1,16 @@
+package org.prac;
+
+public class MasterKishore {
+
+	private void method1() {
+		System.out.println("m1");
+	}
+	private void method2() {
+		System.out.println("m2");
+	}
+	private void method3() {
+		System.out.println("m3");
+
+	}
+
+}
